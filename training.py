0c5fd5b3b346283b9fef2ef252fd57abfa0af000
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2024-09-20 11:04:56 +0200

    Replaced manual csv parse with csv module

--- training.py
+++ training.py
@@ -1,21 +1,16 @@
 from model import LinearRegression
 import random as rand
 import matplotlib.pyplot as plt
+import csv
 
 if __name__ == "__main__" :
     model = LinearRegression()
     training = {'x' : list(), 'y' : list()}
     with open('data.csv', 'r') as f :
-        lines = f.readlines()
-        l     = len(lines) - 1
-        tmp   = [i for i in range(l)]
-        rand.shuffle(tmp)
-        for i in range(1, l) :
-            s = lines[i].split(',')
-            if True: 
-                training['x'].append(float(s[0]))
-                training['y'].append(float(s[1]))
-
+        reader = csv.DictReader(f, delimiter=',')
+        for l in reader:
+                training['x'].append(float(l['km']))
+                training['y'].append(float(l['price']))
     model.train(training['x'], training['y'])
     model.export_params()
     print('RMSE =', model.rmse(training['x'], training['y']))
@@ -27,6 +22,6 @@ if __name__ == "__main__" :
     plt.plot([xmin, xmax], [ymin, ymax], color="red")
     plt.scatter(training['x'], training['y'], color="black", marker="x")
     plt.subplot(1,2,2)
-    plt.plot([i for i in range(model.epoch+1)], model.l_loss)
+    plt.plot(model.l_loss)
     plt.show()
 
